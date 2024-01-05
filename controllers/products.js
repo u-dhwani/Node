39524f9e6914c51ad65bf46dc610f09bb56b4863
Dhwani Upadhyay <sdkkupadhyay002@gmail.com>
2024-01-05 12:08:16 +0530

    Module - 7

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,7 @@
 // const { products } = require("../../routes/admin");
 
-const products=[];
+// const products=[];
+const Product=require('../models/products');
 exports.getAddProduct = (req,res,next) => {
     res.render('add-product', {
         pageTitle: 'Add Product',
@@ -11,17 +12,22 @@ exports.getAddProduct = (req,res,next) => {
     });
 };
 exports.postAddProduct=(req,res,next)=>{
-    products.push({title:req.body.title});
+    const product=new Product(req.body.title);
+    product.save();
+   // products.push({title:req.body.title});
     res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
-    res.render('shop', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/',
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
+    Product.fetchAll((products)=>{
+        res.render('shop', {
+            prods: products,
+            pageTitle: 'Shop',
+            path: '/',
+            hasProducts: products.length > 0,
+            activeShop: true,
+            productCSS: true
+          });
     });
+    
   };
---
+++ models/products.js
@@ -0,0 +1,55 @@
+const fs=require('fs');
+const path=require('path');
+const products=[];
+const p=path.join(process.mainModule.filename,'../','data','products.json');
+const getProductsFromFile=cb=>{
+        fs.readFile(p,(err,fileContent)=>{
+            if(err){
+                cb([]); 
+            }
+            else{
+                cb(JSON.parse(fileContent));    
+            }
+            
+    });
+};
+
+module.exports=class Product{
+    constructor(t){
+        this.title=t;
+    }
+    // function
+    save(){
+        // const p=path.join(process.mainModule.filename,'../','data','products.json');
+        getProductsFromFile(products => { 
+            products.push(this);
+            fs.writeFile(p,JSON.stringify(products),(err)=>{
+                console.log(err);   
+            });
+        } );
+        // fs.readFile(p,(err,fileContent)=>{});
+       
+       // fs.readFile(p,(err,fileContent)=>{
+            // let products=[];
+            // if(!err){
+            //     products=JSON.parse(fileContent);
+            //     // helper object in vanilla node js
+            // }
+            
+       // });
+
+    }
+
+    // storing data in file
+    static fetchAll(cb){
+        getProductsFromFile(cb);
+        // const p=path.join(process.mainModule.filename,'../','data','products.json');
+        // fs.readFile(p,(err,fileContent)=>{
+        //     if(err){
+        //         cb([]); 
+        //     }
+        //     cb(JSON.parse(fileContent));
+        // });
+       // return products;
+    }
+};
